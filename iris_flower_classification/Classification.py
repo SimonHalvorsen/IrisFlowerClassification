b796258fcfe0b75f5c76cd12a1e19bc9949a1767
Simon Halvorsen <halvorsen.simon@gmail.com>
2018-06-01 19:04:53 +0200

    Trained 5 different models on the same data

--- iris_flower_classification/Classification.py
+++ iris_flower_classification/Classification.py
@@ -1,6 +1,11 @@
 import pandas
-import matplotlib.pyplot as plt
 from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 # loading dataset directly from the UCI Machine Learning Repository
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
@@ -17,3 +22,15 @@ seed = 7
 # Splitting dataset into training and test set (X) with corresponding labels (y)
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=validation_size,
                                                                     random_state=seed)
+# Algorithms
+models = [('LR', LogisticRegression()), ('LDA', LinearDiscriminantAnalysis()), ('KNN', KNeighborsClassifier()),
+          ('CART', DecisionTreeClassifier()), ('NB', GaussianNB()), ('SVM', SVC())]
+
+results, names = [], []
+
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, y_train, cv=kfold)
+    results.append(cv_results)
+    names.append(name)
+    print("{}: {} ({})".format(name, cv_results.mean(), cv_results.std()))
